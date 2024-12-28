09f403f40a5826db4abbbf1871c9e2ffc846be81
Acclorite <140836141+Acclorite@users.noreply.github.com>
2024-12-28 11:33:57 +0200

    🛠️ Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -108,12 +108,12 @@ dependencies {
 
     // All dependencies
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
-    implementation("com.google.accompanist:accompanist-swiperefresh:0.34.0")
+    implementation("com.google.accompanist:accompanist-swiperefresh:0.36.0")
 
     // Dagger - Hilt
-    implementation("com.google.dagger:hilt-android:2.52")
-    ksp("com.google.dagger:hilt-android-compiler:2.52")
-    implementation("com.google.dagger:hilt-compiler:2.52")
+    implementation("com.google.dagger:hilt-android:2.54")
+    ksp("com.google.dagger:hilt-android-compiler:2.54")
+    implementation("com.google.dagger:hilt-compiler:2.54")
     ksp("androidx.hilt:hilt-compiler:1.2.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
@@ -131,16 +131,16 @@ dependencies {
     implementation("androidx.core:core-splashscreen:1.0.1")
 
     // Permission handling
-    implementation("com.google.accompanist:accompanist-permissions:0.34.0")
+    implementation("com.google.accompanist:accompanist-permissions:0.37.0")
 
     // PDF parser
     implementation("com.tom-roush:pdfbox-android:2.0.27.0")
 
     // EPUB parser
-    implementation("org.jsoup:jsoup:1.18.1")
+    implementation("org.jsoup:jsoup:1.18.3")
 
     // FB2 parser
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.7.3")
 
     // Language Switcher
     implementation("androidx.appcompat:appcompat:1.7.0")
@@ -152,8 +152,8 @@ dependencies {
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.11.0")
-    implementation("com.squareup.moshi:moshi:1.15.1")
-    implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
+    implementation("com.squareup.moshi:moshi:1.15.2")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.2")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
@@ -162,7 +162,7 @@ dependencies {
     implementation("com.mikepenz:aboutlibraries-compose-m3:11.2.3")
 
     // Drag & Drop
-    implementation("sh.calvin.reorderable:reorderable:2.3.1")
+    implementation("sh.calvin.reorderable:reorderable:2.4.2")
 
     // Scrollbar
     implementation("com.github.nanihadesuka:LazyColumnScrollbar:2.2.0")
@@ -171,5 +171,5 @@ dependencies {
     implementation("com.google.code.gson:gson:2.11.0")
 
     // Markdown
-    implementation("org.commonmark:commonmark:0.23.0")
+    implementation("org.commonmark:commonmark:0.24.0")
 }
