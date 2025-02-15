30a0febf036842fe21be461560fef5e93a3d4bf6
Acclorite <140836141+Acclorite@users.noreply.github.com>
2025-02-15 12:39:25 +0200

    🛠️ Update kotlin version
    * Updated kotlin version to 2.1.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,9 +60,6 @@ android {
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.7"
-    }
     dependenciesInfo {
         includeInApk = false
         includeInBundle = false
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.26" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false
+    id("com.google.devtools.ksp") version "2.1.10-1.0.30" apply false
     id("com.google.dagger.hilt.android") version "2.52" apply false
     id("com.mikepenz.aboutlibraries.plugin") version "11.1.3" apply false
     id("androidx.room") version "2.6.1" apply false
