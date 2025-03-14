c49e7f95006f6c4016896e2c34d79cf83971fab5
Acclorite <140836141+Acclorite@users.noreply.github.com>
2025-03-14 18:05:14 +0200

    🛡️ Release v1.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "ua.acclorite.book_story"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.6.0"
+        versionCode = 12
+        versionName = "1.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
