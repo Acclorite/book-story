a79af2373479a68de36bec46fada70fb97ddec98
Acclorite <2ma0bhpk@anonaddy.com>
2025-10-16 09:06:14 +0300

    fix: downgrade AGP version to 8.11.1
    Resolves: #266

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.13.0" apply false
+    id("com.android.application") version "8.11.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
     id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
